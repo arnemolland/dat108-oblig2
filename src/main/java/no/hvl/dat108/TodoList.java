47e31afb424a20a64f722fcc14abb116c69ce2d6
Arne Olai Lyngsmo Molland <arneolaim@gmail.com>
2018-10-05 14:03:10 +0200

    :coffee: Classes for a Todo-list

---
+++ src/main/java/no/hvl/dat108/TodoList.java
@@ -0,0 +1,16 @@
+package no.hvl.dat108;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoList{
+    private List<TodoItem> items = new ArrayList<>();
+
+    public void addItem(TodoItem item){
+        items.add(item);
+    }
+
+    public List<TodoItem> getItems(){
+        return items;
+    }
+}
